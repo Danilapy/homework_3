ba9bb977a56124c96c56e4da65f2bcb9e7a7a659
Danila <Danila.py@protonmail.com>
2021-10-11 00:31:26 +0500

    done

---
+++ antonenko_danila_dz3_ls3.py
@@ -0,0 +1,20 @@
+from random import randrange
+
+
+nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+
+def get_jokes(n):
+    joke_list = []
+    for joke in range(n):
+        joke = ' '.join([nouns[randrange(len(nouns))], adverbs[randrange(len(adverbs))], adjectives[randrange(len(adjectives))]])
+        joke_list.append(joke)
+        print(joke_list)
+    print(joke_list)
+
+
+
+
+get_jokes(5)
